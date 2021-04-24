4f7abf99edaaf10967926e2ab171514ae4254d64
shareed <shannontlreed@gmail>
2021-04-24 09:45:48 -0700

    revising notes

--- JavaProjects/final/ClassB.java
+++
@@ -1,7 +0,0 @@
-public final class ClassB extends ClassA {
-    
-	//method overloading
-	public final void someMethod(int x){
-	    System.out.println("Some method.");
-	  }
-}
--- JavaProjects/final/Final.java
+++
@@ -1,8 +0,0 @@
-public final class Final {
-    protected final String type = "Type-A";
-    protected final ClassA classA = new ClassA();
-
-    public final void someMethod(){ 
-      System.out.println("Some method");
-    }
-}
